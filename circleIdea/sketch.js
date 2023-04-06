197d94b8593d702fa605138aa58816b0ee013887
TheKooperDon <valodinejr@gmail.com>
2023-04-06 12:33:25 -0500

    trying to change the colors of the circle
    trying to use a if else if statement to show case a change of colors but its not working just yet.

--- circleIdea/sketch.js
+++ circleIdea/sketch.js
@@ -11,11 +11,32 @@ function mousePressed() {
 }
 
 function draw() {
- background(0);
- noStroke();
- fill(255);
- circle(circleX, windowWidth/2, windwoHieght/2);
  
+ background(0)
+ fill(255,255,0);
+ noFill()
+ stroke(255, 204, 0);
+ if (mouseX > 400) {
+  stroke(255,0,0)
+
+ }else if(mouseX >= 500 && mouseX <= 700){
+  stroke(0,255,0)
+
+ }else if(mouseX >= 701 && mouseX <= 1000){
+  stroke(0,0,255)
+
+ }else if(mouseX > 1001 && mouseX <= 1300){
+  stroke(0,255,255)
+
+ }else{
+  stroke(255,0,255)
+ }
+
+
+ ellipse(windowWidth/2, windowHeight/2, 200, 200);
+
+ 
+  
 
 
 }
