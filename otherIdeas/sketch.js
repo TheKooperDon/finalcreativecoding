02fc1867c59f41fe144b25dcfc35d4be45d071ab
TheKooperDon <valodinejr@gmail.com>
2023-04-18 11:16:57 -0500

    a decent if statment
    Helps learn how to make it shorter then a if statement

--- otherIdeas/sketch.js
+++ otherIdeas/sketch.js
@@ -10,14 +10,41 @@ function draw() {
   strokeWeight(4)
   stroke(255);
 
-
-  ellipse(0, 200, 25, 25);
-  ellipse(0, 200, 25, 25);
-  ellipse(0, 200, 25, 25);
-  ellipse(0, 200, 25, 25);
-  ellipse(0, 200, 25, 25);
-  ellipse(0, 200, 25, 25);
-  ellipse(0, 200, 25, 25);
+  for (var x = 0; x <= width; x = x + 50 ){
+    
+    fill(255,0,200)
+    ellipse(x, 200, 25, 25);
+  }
 
 
+
+  /*
+var x = 0;
+  while (x <= width) {
+    xc
+    x = x + 50;
+
+  }
+
+  ellipse(0, 200, 25, 25);
+  x = x + 50;
+
+  ellipse(50, 200, 25, 25);
+  x = x + 50;
+
+  ellipse(100, 200, 25, 25);
+  x = x + 50;
+
+  ellipse(150, 200, 25, 25);
+  x = x + 50;
+
+  ellipse(200, 200, 25, 25);
+  x = x + 50;
+
+  ellipse(250, 200, 25, 25);
+  x = x + 50;
+
+  //ellipse(300, 200, 25, 25);
+
+*/
 }
