d02c9ba121c34494c3f4bfd630875a5c8ac183b9
TheKooperDon <valodinejr@gmail.com>
2023-05-07 16:43:16 -0500

    Update sketch.js

--- test/sketch.js
+++ test/sketch.js
@@ -1,25 +1,64 @@
-let variationNumber = 1; // Change this number to generate a new variation
-let variationSeed; // This will be used to set the random seed for each variation
+
+let bubble1;
+let bubble2;
+let bubble3;
+let bubble4;
+let bubble5;
+let bubble6;
+let bubble7;
 
 function setup() {
-  createCanvas(600, 600);
-  variationSeed = random(10000); // Generate a random seed for this variation
-  randomSeed(variationSeed); // Set the random seed for this variation
+  createCanvas(800, 500);
+  
+  bubble1 = new Bubble();
+  bubble2 = new Bubble();
+  bubble3 = new Bubble();
+  bubble4 = new Bubble();
+  bubble5 = new Bubble();
+  bubble6 = new Bubble();
+  bubble7 = new Bubble();
 }
 
-function draw() {
-  background(220);
 
-  // Your generative art code goes here!
-  // Use the randomSeed() function to ensure the same result each time
-  // when generating the same variation number.
 
-  // For example:
-  fill(random(255), random(255), random(255));
-  ellipse(random(width), random(height), random(50, 100));
 
-  // Save the canvas as an image and add it to an NFT
-  if (frameCount == 1) {
-    saveCanvas(`variation${variationNumber}.jpg`);
+function draw(){
+background(50);
+bubble1.move();
+bubble1.show();
+
+bubble2.move();
+bubble2.show();
+
+bubble3.move();
+bubble3.show();
+
+bubble4.move();
+bubble4.show();
+
+bubble5.move();
+bubble5.show();
+
+bubble6.move();
+bubble6.show();
+
+bubble7.move();
+bubble7.show();
+}
+
+class Bubble{
+  constructor() {
+    this.x=100;
+    this.y=100;
+  }
+  move() {
+    this.x = this.y + random(-10, 10);
+    this.y = this.y + random(-10, 10);
+  }
+  show() {
+    stroke(255);
+    strokeWeight(4);
+    noFill();
+    ellipse(this.x, this.y, 24, 24)
   }
 }
